1d9b68ea978ee797ae6bc92f5bb9792d7d4bd54d
pavanleton <pavankumarkpkp@gmail.com>
2021-06-21 19:18:41 +0530

    add javascript

--- README.md
+++
@@ -1,1 +0,0 @@
-# DomManipulationB1
--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-const taskModal = document.getElementById("exampleModal");
+// Parent element to store cards
+const taskContainer = document.querySelector(".task__container");
 
-const handleSave = (e) => {
-  if (!e) e = window.event;
-  e.preventDefault();
+const newCard = ({
+  id,
+  imageUrl,
+  taskTitle,
+  taskDescription,
+  taskType,
+}) => `<div class="col-md-6 col-lg-4" id=${id}>
+<div class="card">
+  <div class="card-header d-flex justify-content-end gap-2">
+    <button type="button" class="btn btn-outline-success">
+      <i class="fas fa-pencil-alt"></i>
+    </button>
+    <button type="button" class="btn btn-outline-danger">
+      <i class="fas fa-trash-alt"></i>
+    </button>
+  </div>
+  <img
+    src=${imageUrl}
+    class="card-img-top"
+    alt="..."
+  />
+  <div class="card-body">
+    <h5 class="card-title">${taskTitle}</h5>
+    <p class="card-text">
+      ${taskDescription}
+    </p>
+    <span class="badge bg-primary">${taskType}</span>
+  </div>
+  <div class="card-footer text-muted">
+    <button type="button" class="btn btn-outline-primary float-end">
+      Open Task
+    </button>
+  </div>
+</div>
+</div>`;
 
-  // object to store all the task
-  const taskInput = {
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`, // unique number for card id 
     imageUrl: document.getElementById("imageurl").value,
     taskTitle: document.getElementById("tasktitle").value,
     taskType: document.getElementById("tasktype").value,
     taskDescription: document.getElementById("taskdescription").value,
   };
 
-  console.log(taskInput);
+  const createNewCard = newCard(taskData);
+
+  taskContainer.insertAdjacentHTML("beforeend", createNewCard);
 };
