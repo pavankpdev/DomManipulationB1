8c64643a99eeeff2c2186ee6a92066ebe5e5b318
pavanleton <pavankumarkpkp@gmail.com>
2021-06-22 18:52:55 +0530

    add localstorage support

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Parent element to store cards
 const taskContainer = document.querySelector(".task__container");
 
+// Global Store
+const globalStore = [];
+
 const newCard = ({
   id,
   imageUrl,
@@ -37,6 +40,22 @@ const newCard = ({
 </div>
 </div>`;
 
+const loadInitialTaskCards = () => {
+  // access localstorage
+  const getInitialData = localStorage.getItem("tasky"); // null
+  if (!getInitialData) return;
+
+  // convert stringified-object to object
+  const { cards } = JSON.parse(getInitialData);
+
+  // map around the array to generate HTML card and inject it to DOM
+  cards.map((cardObject) => {
+    const createNewCard = newCard(cardObject);
+    taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+    globalStore.push(cardObject);
+  });
+};
+
 const saveChanges = () => {
   const taskData = {
     id: `${Date.now()}`, // unique number for card id
@@ -46,15 +65,20 @@ const saveChanges = () => {
     taskDescription: document.getElementById("taskdescription").value,
   };
 
+  // HTML code
   const createNewCard = newCard(taskData);
 
   taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+  globalStore.push(taskData);
+
+  // add to localstorage
+  localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
 };
 
 // Issues
 
-// The modal was not closing upon adding new card.
-// the cards were deleted after refresh
+// The modal was not closing upon adding new card. [solved]
+// the cards were deleted after refresh  -> localstorage (5MB)
 
 // Features
 // Delete modal feature
