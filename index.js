12218d5b833f4d201dfd4eb40d710467666a0b2d
pavanleton <pavankumarkpkp@gmail.com>
2021-06-27 19:05:56 +0530

    add contenteditable attribute dynamically

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const newCard = ({
 }) => `<div class="col-md-6 col-lg-4" id=${id}>
 <div class="card">
   <div class="card-header d-flex justify-content-end gap-2">
-    <button type="button" class="btn btn-outline-success">
-      <i class="fas fa-pencil-alt"></i>
+    <button type="button" id=${id} class="btn btn-outline-success" onclick="editCard.apply(this, arguments)">
+      <i class="fas fa-pencil-alt" id=${id} onclick="editCard.apply(this, arguments)" ></i>
     </button>
     <button type="button" id=${id} class="btn btn-outline-danger" onclick="deleteCard.apply(this, arguments)">
       <i class="fas fa-trash-alt" id=${id} onclick="deleteCard.apply(this, arguments)"></i>
@@ -104,15 +104,26 @@ const deleteCard = (event) => {
   );
 };
 
-// Features
+const editCard = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
 
-// Edit -> Required
+  let parentElement;
 
-// HINT
+  if (tagname === "BUTTON") {
+    parentElement = event.target.parentNode.parentNode;
+  } else {
+    parentElement = event.target.parentNode.parentNode.parentNode;
+  }
 
-// Complicated yet easy -> 
+  let taskTitle = parentElement.childNodes[5].childNodes[1];
+  let taskDescription = parentElement.childNodes[5].childNodes[3];
+  let taskType = parentElement.childNodes[5].childNodes[5];
+  let submitButton = parentElement.childNodes[7].childNodes[1];
 
-// 1. contenteditable
-// 2. setAttributeNode()
-
-// Open (Optional)
+  taskTitle.setAttribute("contenteditable", "true");
+  taskDescription.setAttribute("contenteditable", "true");
+  taskType.setAttribute("contenteditable", "true");
+  submitButton.innerHTML = "Save Changes";
+};
